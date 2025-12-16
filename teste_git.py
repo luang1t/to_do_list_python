b37fcdc49bb79deedcad312a8a0b948a4bb9f7c3
unknown <luanzragit@gmail.com>
2025-12-16 16:19:05 -0300

    menu CLI finalizado

--- teste_git.py
+++ teste_git.py
@@ -1,1 +1,23 @@
-print("Hello World!")
+def menu():
+    print("""
+  *-*-*-*- MENU -*-*-*-*
+*                        *
+*   1 - Criar            *
+*   2 - Listar           *
+*   3 - Deletar          *
+*   4 - Sair             *
+*                        *
+  -*-*-*-*-*-*-*-*-*-*-*-
+""")
+
+def criar_tarefa():
+    ...
+
+def listar_tarefas():
+    ...
+
+def deletar_tarefa():
+    ...
+
+
+menu()    
