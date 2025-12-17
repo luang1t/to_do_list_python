d3de6d58913baa76ab2ca2e38f9aea6a1feb2043
luanzra <luanzragit@gmail.com>
2025-12-16 23:35:18 -0300

    código funcionando

--- teste_git.py
+++ teste_git.py
@@ -11,13 +11,43 @@ def menu():
 """)
 
 def criar_tarefa():
-    ...
+    tarefa = input("Digite sua tarefa abaixo:\n").split()
+    lista_de_tarefas.append(tarefa)
 
-def listar_tarefas():
-    ...
+def listar_tarefas(lista):
+    if lista_de_tarefas == "":
+      print("Lista Vazia.")
+    else:
+      print("To-Do List:")
+      for id,tarefa in enumerate(lista, start=1):
+        print(f"{id} - {tarefa}")
 
-def deletar_tarefa():
-    ...
+def deletar_tarefa(lista):
+    if lista == "":
+       print("Lista Vazia.")
+    else:   
+      listar_tarefas(lista)
+      deletar = int(input("selecione a tarefa que você deseja deletar ou zero para cancelar a operação: "))
+      if len(lista) >= deletar >= 1:
+         lista.pop(deletar-1)
+      else:
+         print("Fora de alcance.")
+         
+       
+#===================================================================#
 
 
-menu()    
+lista_de_tarefas = list()
+
+while True:
+  menu()
+  seletor = int(input("--> "))
+  if seletor == 1:
+      criar_tarefa()
+  elif seletor == 2:
+      listar_tarefas(lista_de_tarefas)
+  elif seletor == 3:
+      deletar_tarefa(lista_de_tarefas)
+  elif seletor == 4:
+      print("Fim")
+      break    
